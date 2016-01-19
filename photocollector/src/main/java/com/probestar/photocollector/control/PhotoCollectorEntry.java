6bbc3146d03390d1b6fd3b64c2755cfa124db53f
Elvis <elvis@ultrapowersoft.com>
2016-01-19 15:13:57 +0800

    111 

---
+++ photocollector/src/main/java/com/probestar/photocollector/control/PhotoCollectorEntry.java
@@ -0,0 +1,13 @@
+package com.probestar.photocollector.control;
+
+public class PhotoCollectorEntry {
+
+	public static void main(String[] args) {
+		try {
+
+		} catch (Throwable t) {
+
+		}
+	}
+
+}
